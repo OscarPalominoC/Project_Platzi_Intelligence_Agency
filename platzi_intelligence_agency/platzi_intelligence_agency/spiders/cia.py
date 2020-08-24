2279051e97acdfb878dd72ddeb1e028a20c50011
Oscar Palomino <ing.oscarp1@gmail.com>
2020-08-24 01:27:56 -0500

    update

--- platzi_intelligence_agency/platzi_intelligence_agency/spiders/cia.py
+++ platzi_intelligence_agency/platzi_intelligence_agency/spiders/cia.py
@@ -3,6 +3,8 @@ import scrapy
 #XPath
 
 # links = '//a[starts-with(@href, "collection") and (parent::h3|parent::h2)]/@href'
+# title = '//div[@class="documentContent"]/h1[@class="documentFirstHeading"]/text()'
+# body = '//div[@class="field-item even"]/p/text()'
 
 class SpiderCIA(scrapy.Spider):
     name = 'cia'
@@ -20,9 +22,16 @@ class SpiderCIA(scrapy.Spider):
         'FEED_EXPORT_ENCODING': 'utf-8'
     }
     def parse(self, response):
-        links_declassified = response.xpath('//a[starts-with(@href, "collection") and (parent::h3|parent::h2)]/@href'.getall())
+        links_declassified = response.xpath('//a[starts-with(@href, "collection") and (parent::h3|parent::h2)]/@href').getall()
         for link in links_declassified:
-            yield response.follow(link, callback=self.parse_link)
+            yield response.follow(link, callback=self.parse_link, cb_kwargs={'url': response.urljoin(link)})
 
-    def parse_link(self, response):
-        
+    def parse_link(self, response, **kwargs):
+        link = kwargs['url']
+        title = response.xpath('//div[@class="documentContent"]/h1[@class="documentFirstHeading"]/text()').get()
+        paragraph = response.xpath('//div[@class="field-item even"]/p[not(@class)]/text()').getall()
+        yield {
+            'url': link,
+            'title': title,
+            'body': paragraph
+        }
